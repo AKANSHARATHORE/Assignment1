39110657dcace9773decf10d6c3cf7718bb94eb6
AKANSHARATHORE <125644581+AKANSHARATHORE@users.noreply.github.com>
2023-02-17 14:07:31 +0530

    Assignment1 submitted

---
+++ Assignment1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Assignment1
+{
+    public static void main(String ar[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a;
+        System.out.println("Enter number");
+        a=s.nextInt();
+        if(a>0)
+        System.out.println("Positive number");
+        else
+        System.out.println("Negative number");
+    }
+}
